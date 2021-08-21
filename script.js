c4598a4ee0e9180cb40c6afbc7d12b2a8e245f12
StevenLance <44912347+stevenjlance@users.noreply.github.com>
2021-08-21 23:38:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function setup(){
+  // Code here runs only once
+  createCanvas(800, 600)
+}
+
+function draw(){
+  background(220)
+	text(mouseX + " ," + mouseY, 25, 25)
+	// Code logo below here
+
+
+
+
+
+
+
+
+	
+}
